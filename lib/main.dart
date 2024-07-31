d05a111f32b53b63ec0ad052c148b625f97993cf
arunbabuvn <arunbabuvn@gmail.com>
2024-07-31 17:52:10 +0530

    moved to appropriate folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:scheduler_pro/components/pixels.dart';
+import 'package:scheduler_pro/core/theme/app_theme.dart';
 import 'package:scheduler_pro/firebase_options.dart';
-import 'package:scheduler_pro/screens/auth_screen/bloc/auth_bloc.dart';
-import 'package:scheduler_pro/services/router.dart';
+import 'package:scheduler_pro/presentation/screens/home_screen/bloc/home_bloc.dart';
+import 'package:scheduler_pro/presentation/screens/main_screen/bloc/navigation_bloc.dart';
+import 'package:scheduler_pro/presentation/routes/router.dart';
+import 'package:scheduler_pro/presentation/screens/signin_screen/bloc/signin_bloc.dart';
+import 'package:scheduler_pro/presentation/widgets/custom_textfield/cubit/date_and_time_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -18,9 +24,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Pixels.init(context: context);
-    return BlocProvider(
-      create: (context) => AuthBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => SigninBloc(
+            firebaseAuth: FirebaseAuth.instance,
+          ),
+        ),
+        BlocProvider(create: (context) => NavigationBloc()),
+        BlocProvider(create: (context) => HomeBloc()),
+        // BlocProvider(create: (context) => DateAndTimeCubit())
+      ],
       child: ScreenUtilInit(
         designSize: const Size(393, 852),
         minTextAdapt: true,
@@ -28,11 +42,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) => MaterialApp.router(
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            scaffoldBackgroundColor: const Color(0xFFFEFEFE),
-            useMaterial3: true,
-          ),
+          theme: AppTheme.lightTheme,
           routerConfig: appRoute,
         ),
       ),
